5ad7c9423de1c42b772e002eb843bff8b12c0e7f
Conrad Douglas <conraddgls@gmail.com>
2017-02-16 16:40:51 +0900

    HttpRequest Class

---
+++ DHH/DHH/HttpRequest/HttpRequest.cs
@@ -0,0 +1,21 @@
+﻿using WinHttp;
+
+namespace DHH.HttpRequest
+{
+    public class HttpRequest
+    {
+        WinHttpRequest request = new WinHttpRequest();
+
+        public HttpRequest(string Method, string URL, bool Async)
+        {
+            request.Open(Method, URL, Async);
+        }
+
+        public string GetHtmlSource()
+        {
+            request.Send();
+            request.WaitForResponse();
+            return request.ResponseText;
+        }
+    }
+}
